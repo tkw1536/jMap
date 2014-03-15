267fb5e847a499e98d6e6576eb0bc2151c42aca1
sourabhlal <sourabhlal@yahoo.co.in>
2014-03-15 01:15:47 +0100

    College 3 D-block
    Added basic structure, numbers will be added tomorrow

--- data/collegethree.js
+++ data/collegethree.js
@@ -47,12 +47,60 @@ var C3 = (function(){
 
 		[18, 12, 34, 2, "CC3 Corridor", [""], "cor"]
 	];
+
+		var Dblock1 = [
+		[18, -12, 8, 5, "CD1 A/B", ["A/B"], "student"],
+		[26, -12, 8, 5, "CD1 C/D", ["A/B"], "student"],
+		[34, -12, 8, 5, "CD1 E/F", ["A/B"], "student"],
+		[42, -12, 8, 5, "CD1 G/H", ["A/B"], "student"],
+		[48, -12, 2, 5, "CD1 Kitchen", ["Kitchen"], "kitchen"],
+
+		[18, -19, 8, 5, "CD1 I/J", ["A/B"], "student"],
+		[26, -19, 8, 5, "CD1 K/L", ["A/B"], "student"],
+		[34, -19, 8, 5, "CD1 M/N", ["A/B"], "student"],
+		[42, -19, 8, 5, "CD1 O/P", ["A/B"], "student"],
+		[48, -19, 2, 5, "CD1 Stairs", ["Stair"], "stairs"],
+
+		[18, -14, 34, 2, "CD1 Corridor", [""], "cor"]
+	]; 
+
+	var Dblock2 = [
+		[18, -12, 8, 5, "CD2 A/B", ["A/B"], "student"],
+		[26, -12, 8, 5, "CD2 C/D", ["A/B"], "student"],
+		[34, -12, 8, 5, "CD2 E/F", ["A/B"], "student"],
+		[42, -12, 8, 5, "CD2 G/H", ["A/B"], "student"],
+		[48, -12, 2, 5, "CD2 Kitchen", ["Kitchen"], "kitchen"],
+
+		[18, -19, 8, 5, "CD2 I/J", ["A/B"], "student"],
+		[26, -19, 8, 5, "CD2 K/L", ["A/B"], "student"],
+		[34, -19, 8, 5, "CD2 M/N", ["A/B"], "student"],
+		[42, -19, 8, 5, "CD2 O/P", ["A/B"], "student"],
+		[48, -19, 2, 5, "CD2 Stairs", ["Stair"], "stairs"],
+
+		[18, -14, 34, 2, "CD2 Corridor", [""], "cor"]
+	];
+
+	var Dblock3 = [
+		[18, -12, 8, 5, "CD3 A/B", ["A/B"], "student"],
+		[26, -12, 8, 5, "CD3 C/D", ["A/B"], "student"],
+		[34, -12, 8, 5, "CD3 E/F", ["A/B"], "student"],
+		[42, -12, 8, 5, "CD3 G/H", ["A/B"], "student"],
+		[48, -12, 2, 5, "CD3 Kitchen", ["Kitchen"], "kitchen"],
+
+		[18, -19, 8, 5, "CD3 I/J", ["A/B"], "student"],
+		[26, -19, 8, 5, "CD3 K/L", ["A/B"], "student"],
+		[34, -19, 8, 5, "CD3 M/N", ["A/B"], "student"],
+		[42, -19, 8, 5, "CD3 O/P", ["A/B"], "student"],
+		[48, -19, 2, 5, "CD3 Stairs", ["Stair"], "stairs"],
+
+		[18, -14, 34, 2, "CD3 Corridor", [""], "cor"]
+	];
 	return {
-		"name": "College3", 
+		"name": "College 3", 
 		"machine_name": "c3", 
 		"blocks": [
 				{
-					"name": "BlockC", 
+					"name": "Block C+D+E", 
 					"machine_name": "tstC", 
 					"floors": [
 						{
@@ -63,6 +111,15 @@ var C3 = (function(){
 						},
 						{
 							"rooms": Cblock3
+						},
+						{
+							"rooms": Dblock1
+						}, 
+						{
+							"rooms": Dblock2
+						},
+						{
+							"rooms": Dblock3
 						}
 					]
 				}
