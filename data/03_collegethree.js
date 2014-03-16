782b35adc8e06fc7c2415e3e4522cab32c885293
sourabhlal <sourabhlal@yahoo.co.in>
2014-03-16 22:48:02 +0100

    c3 servery

--- data/03_collegethree.js
+++ data/03_collegethree.js
@@ -30,7 +30,8 @@ var C3 = (function(){
 		[18, -14, 34, 2, "CB1 Corridor", [""], "cor"],
 
 		[50, -19, 2, 38, "CAB Corridor", [""], "cor"],
-		[52, 12, 34, 2, "Servery", ["C3-Servery"], "servery"],
+		[52, -19, 12, 12, "Kitchen", [""], "main_kitchen"],
+		[52, -7, 12, 26, "Servery", ["C3-Servery"], "servery"],
 		[64, -19, 2, 38, "CCD Corridor", [""], "cor"]
 	]; 
 
