c28dbb7347981a215ed4e716b37593f1dc9dcdae
sourabhlal <sourabhlal@yahoo.co.in>
2014-03-26 15:27:42 +0100

    Started working on Krupp

--- data/index.js
+++ data/index.js
@@ -1,6 +1,7 @@
 //Index File
 
 //loadBuilding(JS_FILE_NAME, VARIABLE_NAME
+loadBuilding("01_krupp.js", "KP"); 
 loadBuilding("03_collegethree.js", "C3"); 
 loadBuilding("04_nordmetall.js", "NM"); 
 
